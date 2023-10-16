05251496b8c08c94f58d7000e6479ae52ca14c39
Atharva <atharva.deshmukh@siemens.com>
2023-10-16 22:14:00 +0530

    feat: basic DOM methods added

---
+++ JS_files/DOM_Methods.js
@@ -0,0 +1,8 @@
+
+// Print the text inside the <p> tags in the console
+console.log(document.getElementById("p1").innerText);
+console.log(document.getElementById("p1").innerHTML);
+
+// Update the DOM
+document.getElementById("p1").innerText = "Updated Text"
+document.getElementById("p2").innerHTML = "Updated HTML"
