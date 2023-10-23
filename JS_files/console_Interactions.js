ba745e8bc2cb1306cb657d69b9abad83d0776af2
Atharva <atharva.deshmukh@siemens.com>
2023-10-23 20:58:23 +0530

    feat: added consoleInteractions and changed folder structure

--- JS_files/console_Interactions.js
+++ JS_files/console_Interactions.js
@@ -1,4 +1,17 @@
-var input = prompt("Your Input here -> ", "Empty");
-alert(input);
-confirm("Do you want to exit?");
-console.info("for your info");
+/// <reference lib="es2023" />
+
+console.warn('warning');
+console.log('log');
+console.info('info');
+console.error('error');
+
+// Sample data - an array of objects
+const students = [
+  { id: 1, name: 'Alice', grade: 90 },
+  { id: 2, name: 'Bob', grade: 85 },
+  { id: 3, name: 'Charlie', grade: 92 },
+  { id: 4, name: 'David', grade: 78 },
+];
+
+// Using console.table to display the data
+console.table(students);
